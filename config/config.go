c16c0232ac3d938aef73d819d4dd1b6140cdc037
driver005 <paulwolf005@gmail.com>
2024-01-11 10:30:06 +0100

    add config

---
+++ config/config.go
@@ -0,0 +1,67 @@
+package config
+
+import "github.com/spf13/viper"
+
+type Server struct {
+	Port               int    `mapstructure:"port"`
+	Host               string `mapstructure:"host"`
+	ServerName         string `mapstructure:"server_name"`
+	RequestBodyLimit   int    `mapstructure:"request_body_limit"`
+	Timeout            int    `mapstructure:"timeout"`
+	CorsAllowedOrigins string `mapstructure:"cors_allowed_origins"`
+}
+
+type Database struct {
+	Type              string `mapstructure:"type"`
+	Username          string `mapstructure:"username"`
+	Password          string `mapstructure:"password"`
+	Host              string `mapstructure:"host"`
+	Port              int    `mapstructure:"port"`
+	DBname            string `mapstructure:"dbname"`
+	PoolSize          int    `mapstructure:"pool_size"`
+	ConnectionTimeout int    `mapstructure:"connection_timeout"`
+}
+
+type Secrets struct {
+	MasterEncKey string `mapstructure:"master_enc_key"`
+	JwtSecret    string `mapstructure:"jwt_secret"`
+}
+
+type Applictaion struct {
+	Preload bool `mapstructure:"preload"`
+}
+
+type Logger struct {
+	Development bool   `mapstructure:"development"`
+	Level       string `mapstructure:"level"`
+	Encoding    string `mapstructure:"encoding"`
+}
+
+type Config struct {
+	Server         Server      `mapstructure:"server"`
+	MasterDatabase Database    `mapstructure:"master_database"`
+	Secrets        Secrets     `mapstructure:"secrets"`
+	Applictaion    Applictaion `mapstructure:"applictaion"`
+	Logger         Logger      `mapstructure:"logger"`
+}
+
+// LoadConfig reads configuration from file or environment variables.
+func LoadConfig(path string) (config *Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("default")
+	viper.SetConfigType("toml")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	err = viper.Unmarshal(&config)
+	if err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
